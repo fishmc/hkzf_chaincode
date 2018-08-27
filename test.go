e5c76885f750c29ed95c203ca68ac02508ccd323
fishmc <278051663@qq.com>
2018-08-27 03:23:57 -0500

    leo's add house contract transaction

--- test.go
+++ test.go
@@ -9,25 +9,25 @@ type Test struct {
 	//test strct
 }
 
-func (this *Test)Init(stub shim.ChaincodeStubInterface) peer.Response {
+func (this *Test) Init(stub shim.ChaincodeStubInterface) peer.Response {
 	//only one
 	return shim.Success(nil)
 }
 
-func (this *Test)Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+func (this *Test) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	//qufendiaoyongde1fangfa
 	function, parameters := stub.GetFunctionAndParameters()
-	if function == "get"{
-		return this.get(stub,parameters[0])
-	}else if function == "set"{
-		return this.set(stub,parameters[0],[]byte(parameters[1]))
+	if function == "get" {
+		return this.get(stub, parameters[0])
+	} else if function == "set" {
+		return this.set(stub, parameters[0], []byte(parameters[1]))
 	}
 	return shim.Error("Invalid Smart Contract function name.	")
 }
 
 //read shuju
 //GetState(key string) ([]byte, error)
-func (this *Test)get(stub shim.ChaincodeStubInterface,key string) peer.Response{
+func (this *Test) get(stub shim.ChaincodeStubInterface, key string) peer.Response {
 	data, err := stub.GetState(key)
 
 	//chuxianyichang
@@ -35,18 +35,18 @@ func (this *Test)get(stub shim.ChaincodeStubInterface,key string) peer.Response{
 		return shim.Error(err.Error())
 	}
 	//kongshuju
-	if data == nil{
+	if data == nil {
 		return shim.Error("stub shim.ChaincodeStubInterfac")
 	}
 
-	
 	return shim.Success(data)
 }
+
 //xieshuji
 //	PutState(key string, value []byte) error
-func (this *Test)set(stub shim.ChaincodeStubInterface,key string,value []byte) peer.Response{
+func (this *Test) set(stub shim.ChaincodeStubInterface, key string, value []byte) peer.Response {
 	err := stub.PutState(key, value)
-	if err !=nil{
+	if err != nil {
 		shim.Error(err.Error())
 	}
 	return shim.Success(nil)
